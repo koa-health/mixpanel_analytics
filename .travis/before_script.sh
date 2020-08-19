3ac92ff39c6a68671230b03ee4f380239788faac
Roberto Huertas <roberto.huertas@outlook.com>
2020-08-19 15:40:11 +0200

    ci(travis): automate publication

--- .travis/before_script.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -e
-echo "Applying before_script..."
-cd ..
-git clone https://github.com/flutter/flutter.git
-export PATH=`pwd`/flutter/bin:`pwd`/flutter/bin/cache/dart-sdk/bin:$PATH
-cd $TRAVIS_BUILD_DIR
